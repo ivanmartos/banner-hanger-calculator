4488cf449e3a4d5c8b6c37a7b7f92193f86b961f
Ivan Martos <martos.ivan@gmail.com>
2017-07-09 15:58:05 +0200

    hiding calculator on touch of scrollview

--- app/src/main/java/com/martos/bannerhangercalculator/CalculatorActivityFragment.java
+++ app/src/main/java/com/martos/bannerhangercalculator/CalculatorActivityFragment.java
@@ -1,13 +1,17 @@
 package com.martos.bannerhangercalculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import com.martos.bannerhangercalculator.calculator.CalculatedPadding;
@@ -45,8 +49,16 @@ public class CalculatorActivityFragment extends Fragment {
 
         resultRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         resultRecyclerView.setAdapter(resultAdapter);
-    }
 
+        resultRecyclerView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return false;
+            }
+        });
+    }
 
     @OnTextChanged({R.id.paddingEditText, R.id.bannerWidthEditText})
     public void onButtonClick() {
